8b6e0af56a94994abc8c960e7a1b82d45dbd087c
crysislinux <crysis.linux@gmail.com>
2016-07-15 11:55:55 +0800

    sync LDAP realname when change realname

--- src/applications/people/controller/PhabricatorPeopleProfileEditController.php
+++ src/applications/people/controller/PhabricatorPeopleProfileEditController.php
@@ -3,6 +3,24 @@
 final class PhabricatorPeopleProfileEditController
   extends PhabricatorPeopleProfileController {
 
+  public function syncLdapRealName($username, $realname) {
+    $gugud_ldap_connect = ldap_connect("gugud.com");  // assuming the LDAP server is on this host
+    if ($gugud_ldap_connect) {
+      // bind with appropriate dn to give update access
+      ldap_set_option($gugud_ldap_connect, LDAP_OPT_PROTOCOL_VERSION, 3);
+      $gugud_r = ldap_bind($gugud_ldap_connect, "cn=admin,dc=gugud,dc=com", "ts3qdf");
+
+      // prepare data
+      $gugud_user_entry["cn"] = $realname;
+      // add data to directory
+      $gugud_r = ldap_modify($gugud_ldap_connect, "uid=" . $username . ",ou=people,dc=gugud,dc=com", $gugud_user_entry);
+
+      ldap_close($gugud_ldap_connect);
+    } else {
+      echo "caonnot connect to server\n";
+    }
+  }
+
   public function handleRequest(AphrontRequest $request) {
     $viewer = $this->getViewer();
     $id = $request->getURIData('id');
@@ -43,6 +61,15 @@ final class PhabricatorPeopleProfileEditController
         ->setContentSource(
           PhabricatorContentSource::newFromRequest($request))
         ->setContinueOnNoEffect(true);
+
+      $realname = $user->getRealName();
+      for($i = 0; $i < count($xactions); $i++) {
+        if ($xactions[$i]->getMetadataValue('customfield:key') == 'user:realname') {
+          $realname = $xactions[$i]->getNewValue();
+          break;
+        }
+      }
+      $this->syncLdapRealName($user->getUserName(), $realname);
 
       try {
         $editor->applyTransactions($user, $xactions);
