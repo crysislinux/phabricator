3ba44f29a29c04e04fc91b89a8cacb5a27c32a1a
crysislinux <crysis.linux@gmail.com>
2016-07-15 11:11:40 +0800

    add LDAP account when register

--- src/applications/auth/controller/PhabricatorAuthRegisterController.php
+++ src/applications/auth/controller/PhabricatorAuthRegisterController.php
@@ -7,6 +7,35 @@ final class PhabricatorAuthRegisterController
     return false;
   }
 
+  public function addToLDAP($value_username, $value_realname, $value_password) {
+    $gugud_ldap_connect = ldap_connect("gugud.com");  // assuming the LDAP server is on this host
+
+    if ($gugud_ldap_connect) {
+      // bind with appropriate dn to give update access
+      ldap_set_option($gugud_ldap_connect, LDAP_OPT_PROTOCOL_VERSION, 3);
+      $gugud_r = ldap_bind($gugud_ldap_connect, "cn=admin,dc=gugud,dc=com", "ts3qdf");
+
+      // prepare data
+      $gugud_user_entry["uid"] = $value_username;
+      $gugud_user_entry["cn"] = $value_realname;
+
+      $gugud_user_entry["userpassword"] = $value_password;
+      $gugud_user_entry["sn"] = "Jones";
+      $gugud_user_entry["givenname"] = "Jones";
+      $gugud_user_entry["objectclass"][0] = "inetOrgPerson";
+      $gugud_user_entry["objectclass"][1] = "organizationalPerson";
+      $gugud_user_entry["objectclass"][2] = "person";
+      $gugud_user_entry["objectclass"][3] = "top";
+
+      // add data to directory
+      $gugud_r = ldap_add($gugud_ldap_connect, "uid=" . $value_username . ",ou=people,dc=gugud,dc=com", $gugud_user_entry);
+
+      ldap_close($gugud_ldap_connect);
+    } else {
+      echo "caonnot connect to server\n";
+    }
+  }
+
   public function handleRequest(AphrontRequest $request) {
     $viewer = $this->getViewer();
     $account_key = $request->getURIData('akey');
@@ -336,6 +365,8 @@ final class PhabricatorAuthRegisterController
           } else {
             $allow_reassign_email = false;
           }
+
+          $this->addToLDAP($value_username, $value_realname, $value_password);
 
           $user->openTransaction();
 
